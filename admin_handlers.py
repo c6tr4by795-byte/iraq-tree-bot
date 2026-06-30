2216c90a0610a2732eba0851acaee8beabeaa4cb
c6tr4by795-byte <c6tr4by795@privaterelay.appleid.com>
2026-06-30 15:54:38 +0300

    Update admin_handlers.py

--- admin_handlers.py
+++ admin_handlers.py
@@ -1,28 +1,49 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from request_tree import users
+from qr import create_qr
+
+
+tree_number = 1
 
 
 async def admin_buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global tree_number
+
     query = update.callback_query
     await query.answer()
 
     data = query.data
 
     if data.startswith("approve_"):
+
         user_id = int(data.split("_")[1])
 
-        await context.bot.send_message(
+        tree_id = f"IRQ-{tree_number:06d}"
+        tree_number += 1
+
+        qr = create_qr(tree_id)
+
+        await context.bot.send_photo(
             chat_id=user_id,
-            text="✅ تمت الموافقة على طلبك.\n\nسيتم التواصل معك قريباً لاستلام الشتلة."
+            photo=qr,
+            caption=f"""🎉 تمت الموافقة على طلبك.
+
+🌳 رقم الشتلة:
+{tree_id}
+
+📦 هذا هو QR الخاص بالشتلة.
+
+سيتم إعلامك بموقع وموعد الاستلام قريباً.
+"""
         )
 
         await query.edit_message_text(
-            "✅ تمت الموافقة على الطلب."
+            f"✅ تمت الموافقة.\n\nرقم الشتلة: {tree_id}"
         )
 
     elif data.startswith("reject_"):
+
         user_id = int(data.split("_")[1])
 
         await context.bot.send_message(
