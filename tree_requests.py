e2e724dc13635a0c7b52f3d5aed1a8283b68ba79
c6tr4by795-byte <c6tr4by795@privaterelay.appleid.com>
2026-06-30 14:21:40 +0300

    Update tree_requests.py

--- tree_requests.py
+++ tree_requests.py
@@ -3,16 +3,4 @@ from telegram.ext import ContextTypes
 
 
 async def request_data(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text
-
-    if ":" not in text:
-        return
-
-    await update.message.reply_text(
-        f"""✅ وصلت الدالة بنجاح
-
-📋 البيانات التي استلمتها:
-
-{text}
-"""
-    )
+    await update.message.reply_text("🔥 أي رسالة وصلتني")
