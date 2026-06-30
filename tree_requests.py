34e50a4c6751e291f851a6c50f153a47a6cbce7a
c6tr4by795-byte <c6tr4by795@privaterelay.appleid.com>
2026-06-30 13:56:12 +0300

    Update tree_requests.py

--- tree_requests.py
+++ tree_requests.py
@@ -1,8 +1,6 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from config import ADMIN_ID
-
 
 async def request_data(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
@@ -10,24 +8,11 @@ async def request_data(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if ":" not in text:
         return
 
-    # رسالة للمشرف
-    
-        await update.message.reply_text("وصلت الدالة")
-        طلب شتلة جديدة
+    await update.message.reply_text(
+        f"""✅ وصلت الدالة بنجاح
 
-👤 المستخدم:
-{update.effective_user.full_name}
-
-🆔 ID:
-{update.effective_user.id}
-
-📋 البيانات:
+📋 البيانات التي استلمتها:
 
 {text}
 """
-    )
-
-    # رسالة للمستخدم
-    await update.message.reply_text(
-        "✅ تم إرسال طلبك إلى المشرف.\n\nسيتم التواصل معك بعد مراجعة الطلب."
     )
