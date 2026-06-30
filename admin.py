9a66f2a084d4775d7b5086a652be3f07d9c8d762
c6tr4by795-byte <c6tr4by795@privaterelay.appleid.com>
2026-06-30 16:08:11 +0300

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,16 +1,17 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-def admin_keyboard(user_id):
+def admin_keyboard(request_id):
+
     keyboard = [
         [
             InlineKeyboardButton(
                 "✅ موافقة",
-                callback_data=f"approve_{user_id}"
+                callback_data=f"approve:{request_id}"
             ),
             InlineKeyboardButton(
                 "❌ رفض",
-                callback_data=f"reject_{user_id}"
+                callback_data=f"reject:{request_id}"
             ),
         ]
     ]
